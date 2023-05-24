2db5e2003c8fa80b9843af6950f07316b3aefffd
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-05-24 04:14:39 +0300

    refactored repo

---
+++ class1/dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Enter first number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA == numB)
+    {Console.WriteLine("Pointed nubers are same");}
+else if (numA > numB)
+    {Console.WriteLine($"max={numA}, min={numB}");}
+else
+    {Console.WriteLine($"max={numB}, min={numA}");}
---
+++ class1/dz4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Поменяю условие, чтобы было хоть немного интереснее. Все четные от 0 до Z, где Z принадлежит множеству целых чисел
+
+Console.Write("Please, put a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 0)
+    {
+    for (int start = 0; start <= num; start += 2)  //0 - even number!!!
+        {   string end = "";
+            if (start + 2 > num)
+                {end = "";}
+            else
+                {end = ", ";}
+            Console.Write($"{start}{end}");
+        }
+    }
+else
+    {
+    for (int start = 0; start >= num; start -= 2)  //0 - even number!!!
+        {   string end = "";
+            if (start - 2 < num)
+                {end = "";}
+            else
+                {end = ", ";}
+            Console.Write($"{start}{end}");
+        }   
+    }
+
