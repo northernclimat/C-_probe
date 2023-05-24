3325e7a0948601466fe844e464b1e34e93f6f9db
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-05-24 04:29:15 +0300

    made 2nd

---
+++ class2/dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Please, input a number: ");
+string num = Console.ReadLine();
+int lenght = num.Length;
+if (lenght < 3)
+    {
+        Console.WriteLine("The number doesn't have third digit");
+    }
+else
+    {
+        Console.WriteLine($"The 3rd digit of your number is {num[2]}");
+    }
