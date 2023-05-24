85266b62d897dcfe391342b431cb3b2829763ef0
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-05-24 04:08:59 +0300

    made 1th

---
+++ class2/dz1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Please, input a number: ");
+string num = Console.ReadLine();
+Console.WriteLine(num[1]);
