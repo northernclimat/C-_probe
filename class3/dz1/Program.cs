f17ddbe03406cb9c06c6f6f2c30f258e5b831fcd
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-05-29 01:38:24 +0300

    made 3-1

---
+++ class3/dz1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//! Не интересно. Сформулируем так: Напишите программу, которая принимает на вход набор символов и проверяет, является ли он палиндромом
+
+Console.WriteLine("Please, input your collection of symbols: ");
+string str = Console.ReadLine();
+
+if (IsPalindrome(str))
+{
+    Console.WriteLine("Palindrome");
+}
+else
+{
+    Console.WriteLine("Not a palindrome");
+}
+
+bool IsPalindrome(string n) {
+        string trimmed = string.Concat(n.Where(c => !Char.IsWhiteSpace(c))); //* We won't calculate whitespaces in order to parce strings correctly(example "А роза упала на лапу Азора")
+        return trimmed.ToLower().Reverse().SequenceEqual(trimmed);
+    }
