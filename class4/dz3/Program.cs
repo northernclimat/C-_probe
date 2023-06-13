d53a71570574f6aa98d1c2b77d5938fca9a89d68
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-13 18:11:41 +0300

    made 4-3

---
+++ class4/dz3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] GetArrayFromConsole()
+{
+  Console.WriteLine("Please, input the numbers via space, e.g. 1 3 5 6:");
+  int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+  return nums;
+}
+
+Console.WriteLine($"[{string.Join(", ", GetArrayFromConsole())}]");
