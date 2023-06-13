8fef2302d082a392349e7cb6745dfa6142e08651
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-13 17:50:39 +0300

    made 4-2

---
+++ class4/dz2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//! так как требования вычислять корень числа нет - это не рекурсия
+
+int GetANumberFromConsole()
+{
+    Console.WriteLine("Please, input your number:");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int GetaSumofDigits(int a)
+{
+    int num = a.ToString().Sum(e => int.Parse(e.ToString()));
+
+    return a > 9 ? num : a;
+}
+
+Console.WriteLine(GetaSumofDigits(GetANumberFromConsole()));
