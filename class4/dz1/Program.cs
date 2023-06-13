38947a1db897924b43ef558debb357d891458c70
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-13 17:26:02 +0300

    made 4-1

---
+++ class4/dz1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//? Решение функцией
+
+double PutNumToDegree()
+{
+    Console.WriteLine("Please, input  number and degree, e.g. 1 3:");
+    double [] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+    
+    return Math.Pow(nums[0], nums[1]);
+}
+
+Console.WriteLine(PutNumToDegree());
+
