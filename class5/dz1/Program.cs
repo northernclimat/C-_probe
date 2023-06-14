6d8837f4591e5ea74f6d5da84417d86289f4158e
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-14 04:54:26 +0300

    made 5-1

---
+++ class5/dz1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int num)
+{
+    int[] newArray = Enumerable.Range(100, num).Select(s => new Random().Next(100, 999)).ToArray();
+    return newArray;
+}
+
+Console.WriteLine(string.Join(" ", GetArray(10)));
+
+int SumOfOddNumbers(int[] arr)
+{   
+    int sum = 0;
+    foreach (int el in arr)
+    {
+       if (el % 2 == 0)
+       {
+        sum += 1;
+       }
+    }
+    return sum;
+}
+
+Console.WriteLine("Please, input the lenght of your array:");
+int lenght = int.Parse(Console.ReadLine());
+int [] arr = GetArray(lenght);
+Console.WriteLine($"Массив: {string.Join(" ", arr)}, кол-во четных элементов: {SumOfOddNumbers(arr)}");
