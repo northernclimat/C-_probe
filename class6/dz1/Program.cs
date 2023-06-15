1ef015d2f39eb3be9cbcd46082d999263e028ec3
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-15 03:56:55 +0300

    made 6-1

---
+++ class6/dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void GetArrayLenght()
+{   
+    Console.WriteLine("Please, input the numbers via spaces, e.g. 1 2 3");
+    int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).Where(s => s > 0).ToArray();
+    Console.WriteLine($"Quantity of numbers which are greater than 0 are: {nums.Length}");
+}
+
+GetArrayLenght();
