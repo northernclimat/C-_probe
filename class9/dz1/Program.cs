01640369a0cd3c73499416c2f897cdcd1af2a9e1
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-06-17 04:43:16 +0300

    made 9-1

---
+++ class9/dz1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int InputaMeasure(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+
+
+string ShowNumbers(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    return n + " " + ShowNumbers(n - 1);
+}
+
+int n = InputaMeasure("Please, input upper range: ");
+Console.WriteLine(ShowNumbers(n));
